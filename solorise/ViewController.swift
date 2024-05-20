a20cbaf2af2b59b107c4e6a660da92b21b9dc655
navid-sheikh <navidsheikh2020@gmail.com>
2024-05-20 21:09:29 +0100

    Initial Commit

---
+++ solorise/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  solorise
+//
+//  Created by Navid Sheikh on 20/05/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
